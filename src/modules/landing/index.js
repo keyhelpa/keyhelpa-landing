a0edc1c55091eff4a8cbd4672e962d122d1ef245
kmlcburce <45752669+kmlcburce@users.noreply.github.com>
2022-04-04 13:54:52 +0800

    push

--- src/modules/landing/index.js
+++ src/modules/landing/index.js
@@ -4,7 +4,9 @@ export class Landing extends Component {
   render() {
     return (
       <div className="banner-agent">
-        <h1>fdsfsd</h1>
+        <img style={{
+          width: '100%',
+        }} src="assets/agent-bg.png"></img>
       </div>
     )
   }
