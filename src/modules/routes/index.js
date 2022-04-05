64b092f516d99cc7b011cd282687a7091332474a
pathran-phantom1823 <patrick.pathran23@gmail.com>
2022-04-05 19:37:22 +0800

    updates

--- src/modules/routes/index.js
+++ src/modules/routes/index.js
@@ -3,6 +3,7 @@ import { Switch} from 'react-router-dom';
 import Homepage from 'modules/content/index.js'
 import Landing from 'modules/landing/index.js'
 import About from 'modules/about/index.js'
+import Contacts from 'modules/contacts/index.js'
 import Route from './route'
 function RouteList() {
   return (
@@ -11,6 +12,8 @@ function RouteList() {
       <Route path="/agents" exact component={Landing}></Route>
       <Route path="/agent/about" exact component={About}></Route>
       <Route path="/helpa/about" exact component={About}></Route>
+      <Route path="/agent/contact" exact component={Contacts}></Route>
+      <Route path="/helpa/contact" exact component={Contacts}></Route>
     </Switch>
   )
 }
