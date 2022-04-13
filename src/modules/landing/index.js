e3fe55f49ecf89016d23d64e5b54ee16e8819cf4
kmlcburce <45752669+kmlcburce@users.noreply.github.com>
2022-04-13 15:09:05 +0800

    bottom section

--- src/modules/landing/index.js
+++ src/modules/landing/index.js
@@ -79,6 +79,7 @@ export class Landing extends Component {
             <iframe width="300" height="250" src="https://www.youtube.com/embed/iqlH4okiQqg?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </Grid>
        </Grid>
+
        <Grid style={{
           backgroundColor: '#E5E5E5',
           backgroundImage: `url(${bgAgent})`,
@@ -134,10 +135,93 @@ export class Landing extends Component {
               <h3 style={{
                   color: '#34475DA3'
                 }}>KeyHelpa's payment system releases payment once job milestones have been completed to satisfaction and authorization to release funds confirmed.</h3>
-              
+            </Grid>
+          </Grid>
+        
+          <Grid style={{
+          backgroundColor: '#E5E5E5',
+          backgroundImage: `url(${bgAgent})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          width: '100%'
+        }} container alignItems={'left'} justifyContent={'left'}>
+
+            <Grid item xs={6} style={{
+              padding: '5%',
+              textAlign: 'center'
+            }}>
+              <img style={{
+              width: '80%',
+              height: 'auto',
+            }} src={require('../../assets/agent-i-1.png')}></img>
+            </Grid>
+            <Grid item xs={6} style={{
+              padding: '5% 5% 5% 5%',
+              textAlign: 'left'
+            }}>
+              <h2 style={{
+                color: ' #34475D'
+              }}>Find the right help.</h2>
+              <h2 style={{
+                color: ' #34475D'
+              }}>Right Now.</h2>
+              <p style={{
+                color: '#34475DA3',
+                fontSize: '18px'
+              }}>Quickly and easily access a pool of available and freelancers who are familiar with the real estate and property management industry, the suburbs where you need work done and the job that needs doing. Search profiles for a freelance or browse by skills, years of experience, area, service and more. Then select the type of help and price that's right for you and your agency. No more full-time wages that you don't need to pay - and more money saved.</p>
+            </Grid>
+            
+            
+            <Grid item xs={6} style={{
+              padding: '5% 5% 5% 5%',
+              textAlign: 'left'
+            }}>
+              <h2 style={{
+                color: ' #34475D'
+              }}>Total Control.</h2>
+              <h2 style={{
+                color: ' #34475D'
+              }}>Cosntant Contact.</h2>
+              <p style={{
+                color: '#34475DA3',
+                fontSize: '18px'
+              }}>Send instant messages to the person you choose as your Helpa. Once a job has been offered and accepted, you can automatically privately message one another so you’re in constant contact throughout. You’re in total control to communicate as and when you need to.</p>
+            </Grid>
+            <Grid item xs={6} style={{
+              padding: '5%',
+              textAlign: 'center'
+            }}>
+              <img style={{
+              width: '80%',
+              height: 'auto',
+            }} src={require('../../assets/agent-i-2.png')}></img>
+            </Grid>
+
+            <Grid item xs={6} style={{
+              padding: '5%',
+              textAlign: 'center'
+            }}>
+              <img style={{
+              width: '80%',
+              height: 'auto',
+            }} src={require('../../assets/agent-i-3.png')}></img>
+            </Grid>
+            <Grid item xs={6} style={{
+              padding: '5%',
+              textAlign: 'left'
+            }}>
+              <h2 style={{
+                color: ' #34475D'
+              }}>Your investment.</h2>
+              <h2 style={{
+                color: ' #34475D'
+              }}>Protected.</h2>
+              <p style={{
+                color: '#34475DA3',
+                fontSize: '18px'
+              }}>Once your job has been accepted by a Helpa, the agreed payment is securely held by Key Helpas' payment system. Only once the job has been completed to your satisfaction, do you authorise for your Helpa's funds to be released. With the touch of a button, you can approve for a payment to be automatically distributed in the next payment run. So you can get the help you need knowing that your investment is protected.</p>
             </Grid>
         </Grid>
-
         <Footer/>
       </div>
     )
