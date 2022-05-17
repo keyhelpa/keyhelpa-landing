90e4830a941a1b2b85f8d10c8266a36bf284bcfd
pathran-phantom1823 <patrick.pathran23@gmail.com>
2022-05-17 13:49:07 +0800

    updates

--- src/common/Routes.js
+++ src/common/Routes.js
@@ -1,10 +1,10 @@
 const liveUrl = "https://api.sandbox.keyhelpa.com/public/increment/v1"
 const localUrl = "http://localhost/keyhelpa/keyhelpa-api/public/increment/v1"
-const isDev = true
+const isDev = false
 const baseUrl = isDev ? localUrl : liveUrl
 
 export default{
-  retrievePayload: baseUrl + '/payloads/retrieve',
+  payloadsRetrieve: baseUrl + '/payloads/retrieve',
   createContact: baseUrl + '/contact_us/create',
   freelancer: 'https://sandbox.keyhelpa.com',
   agent: 'https://agent.sandbox.keyhelpa.com',
--- src/modules/about/index.js
+++ src/modules/about/index.js
@@ -12,16 +12,37 @@ export class About extends Component {
     this.state={
       theme: 'agent',
       data: null,
-      isLoading: false
+      isLoading: false,
+      aboutUs: null
     }
   }
   componentDidMount() {
     const {history} = this.props
+    this.retrieve()
     if(history.location.pathname.includes('agent')) {
       this.setState({theme: 'agent'})
     }else{
       this.setState({theme: 'helpa'})
     }
+  }
+
+  retrieve(){
+    const {user} = this.props.state
+    let params = {
+      condition:[
+        {
+          column: 'payload',
+          clause: '=',
+          value: 'about_us'
+        }
+      ]
+    }
+
+    API.request(Routes.payloadsRetrieve, params, response => {
+      if(response.data.length > 0){
+        this.setState({aboutUs: response.data[0]})
+      }
+    })
   }
   renderContent(){
     const {theme, isLoading, data} = this.state
@@ -46,19 +67,23 @@ export class About extends Component {
             </div>
             <div className="column-75 content-left">
               <h1 className={theme==='agent' ? 'agent' : 'helpa'}>About Us</h1>
-              <p>
-                KeyHelpa originated from the question of how to improve the profitability of real estate agencies given the high cost of labour and overhead expenses, employment regulations and the competitive nature of the real estate industry.<br/><br/> The founders bring to the table their knowledge of accountancy, law and real estate practice to provide agency principals with the flexibility and versatility to deal with their high-volume activities without the need to undertake expensive employment and recruitment expenses. It allows experienced real estate personnel the flexibility to choose their working times to suit their own individual lifestyles.
-              </p>
+              {
+                this.state.aboutUs !== null && (
+                  <p dangerouslySetInnerHTML={{__html: this.state.aboutUs?.payload_value}}></p>
+                )
+              }
             </div>
           </section>
       </div>
     )
   }
   render() {
-    const {theme, isLoading, data} = this.state
+    const {theme, isLoading, data, aboutUs} = this.state
     return (
       <div>
-        {this.renderContent()}
+        {
+          this.renderContent()
+        }
         <Footer/>
       </div>
     )
