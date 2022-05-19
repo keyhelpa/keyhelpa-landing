d947c5b63552988c97bd89c4f7ce2f2e9bef5abe
kmlcburce <45752669+kmlcburce@users.noreply.github.com>
2022-05-19 16:36:01 +0800

    updated video section to fetch from db

--- src/modules/landing/index.js
+++ src/modules/landing/index.js
@@ -58,67 +58,85 @@ export class Landing extends Component {
     const {data} = this.state;
     return(
       <div>
-        {
-          data.map((item, index)=> {
-            if(item.payload_value.helps != null){
-              <Grid style={{
-                backgroundColor: '#E5E5E5',
-                backgroundImage: `url(${bgAgent})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center',
-                width: '100%'
-              }} container alignItems={'center'} justifyContent={'center'} key={index}>
-                  <Grid item xs={5} style={{
-                    padding: '5%',
-                    textAlign: 'left'
+        <Grid style={{
+          backgroundColor: '#E5E5E5',
+          backgroundImage: `url(${bgAgent})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          width: '100%'
+        }} container alignItems={'center'} justifyContent={'center'} >
+            <Grid item xs={6} style={{
+              padding: '5%',
+              textAlign: 'left'
+            }}>
+                <h1 className='h1-lg' style={{
+                  fontSize: '80px',
+                  fontWeight: 'bold',
+                  color: '#34475D',
+                }}>What do you need a Helpa for?</h1>
+                <h3 style={{
+                  color: '#34475DA3'
+                }}>Do you need help with your property opens this weekend? Need more manpower to complete your inspection reports? Let KeyHelpa find the freelance help you need.</h3>
+            </Grid>
+            <Grid item style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+            }} xs={6} textAlign={'right'} padding={'5%'} direction={'row'}>
+              {
+                data.map((item, index) => {
+                if(item.payload_value.helps != undefined){
+                  return(
+                  <div key={index} style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center', 
+                    flexBasis: '100%',
                   }}>
-                      <h1 className='h1-lg' style={{
-                        fontSize: '80px',
-                        fontWeight: 'bold',
-                        color: '#34475D',
-                      }}>What do you need a Helpa for?</h1>
+                    <Box sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center', 
+                      flexDirection: 'column',
+                      marginRight: '25px'
+                    }}>
                       <h3 style={{
-                        color: '#34475DA3'
-                      }}>Do you need help with your property opens this weekend? Need more manpower to complete your inspection reports? Let KeyHelpa find the freelance help you need.</h3>
-                  </Grid>
-                  <Grid item xs={3} textAlign={'right'} padding={'5%'} direction={'column'}>
-                    <Grid item xs={5}>
-                    <h3 style={{
                         fontWeight: 'bold',
                         fontSize: '20px',
                         color: '#34475D'
-                      }}>{item.payload_value.helps.title}</h3>
-                      {(item.payload_value.helps.description).map((val, index)=>{
-                        <p key={index}>{val}</p>
+                      }} >{item.payload_value.helps.title}</h3>
+                      {(item.payload_value.helps.description).map((val, ctr)=>{
+                        return(<p key={ctr}>{val}</p>)
                       })}
-                    </Grid>
-                    <Grid item xs={5} style={{marginTop: '25px'}}>
-                    <h3 style={{
-                        fontWeight: 'bold',
-                        fontSize: '20px',
-                        color: '#34475D'
-                      }}>Sales Assistance Help</h3>
-                      <p>Open for inspections</p>
-                      <p>Admin Support</p>
-                      <p>Marketing</p>
-                    </Grid>
-        
-                  </Grid>
-                  <Grid item xs={3} textAlign={'center'} padding={'5%'} >
-                    <div style={{marginTop: '25px'}}>
-                      <iframe  width="300" height="250" src="https://www.youtube.com/embed/iqlH4okiQqg?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
-        
-                  <iframe width="300" height="250" src="https://www.youtube.com/embed/iqlH4okiQqg?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                  </Grid>
-             </Grid>
-            }
-            
-          })
-        }
+                    </Box>
+                    <Box sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center', 
+                      flexDirection: 'row'
+                    }}>
+                      <div style={{
+                          marginTop: '25px',
+                        }}>
+                          <iframe  width="300" height="250" src={item.payload_value.helps.url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </Box> 
+                  </div>
+                  )
+                }
+              })
+              }
+             
+            </Grid>
+            {/* <Grid item xs={3} textAlign={'center'} padding={'5%'} >
+              <div style={{marginTop: '25px'}}>
+                <iframe  width="300" height="250" src="https://www.youtube.com/embed/iqlH4okiQqg?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+  
+            <iframe width="300" height="250" src="https://www.youtube.com/embed/iqlH4okiQqg?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </Grid> */}
+        </Grid>
      </div>
-    );
-    
+      );
   }
   renderFeatures(){
     const {theme} =  this.state;
