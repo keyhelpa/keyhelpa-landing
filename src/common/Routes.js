2c7dcefab4b05dcba7a8108dfb5326669a4c5307
pathran-phantom1823 <patrick.pathran23@gmail.com>
2022-05-23 15:52:36 +0800

    updates

--- src/common/Routes.js
+++ src/common/Routes.js
@@ -1,11 +1,12 @@
-const liveUrl = "https://api.sandbox.keyhelpa.com/public/increment/v1"
-const localUrl = "http://localhost/keyhelpa/keyhelpa-api/public/increment/v1"
-const isDev = false
-const baseUrl = isDev ? localUrl : liveUrl
+// const liveUrl = "https://api.sandbox.keyhelpa.com/public/increment/v1"
+// const localUrl = "http://localhost/keyhelpa/keyhelpa-api/public/increment/v1"
+// const isDev = false
+// const baseUrl = isDev ? localUrl : liveUrl
+import CONFIG from 'config'
 
 export default{
-  payloadsRetrieve: baseUrl + '/payloads/retrieve',
-  createContact: baseUrl + '/contact_us/create',
+  payloadsRetrieve: CONFIG.BACKEND_URL + '/payloads/retrieve',
+  createContact: CONFIG.BACKEND_URL + '/contact_us/create',
   freelancer: 'https://sandbox.keyhelpa.com',
   agent: 'https://agent.sandbox.keyhelpa.com',
 }
