a204215025ec30ef24cbb1b1cdc6016a00a6674c
kennck <kennettecanales@gmail.com>
2022-07-09 15:41:21 +0800

    update

--- src/modules/landing/index.js
+++ src/modules/landing/index.js
@@ -11,90 +11,55 @@ import Routes from 'common/Routes'
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { Color } from 'common'
-import {agent, helpa} from './data'
+import Helper from './helper'
 
 class Landing extends Component {
   constructor(props) {
     super(props)
-    this.state={
+    this.state = {
       theme: 'agent',
-      data: [],
-      isLoading: false,
-      hasHelps: false,
-      hasFeatures: false,
-      hasOthers: false,
-      othersData: [],
-      featuresData: [],
       accountType: null
     }
   }
 
   componentDidMount() {
-    const {setColor, setSelectedUser, history} = this.props
-    const {data} = this.state
+    const { setColor, setSelectedUser, history } = this.props
+    let user = history.location.pathname.includes('agent') ? 'agent' : 'helpa'
     this.setState({
-      accountType: history.location.pathname.includes('agent') ? 'agent' : 'helpa'
+      accountType: user
     })
-    setColor(history.location.pathname.includes('agent') ? 'agent' : 'helpa')
-    setSelectedUser(history.location.pathname.includes('agent') ? 'agent' : 'helpa')
+    setColor(user)
+    setSelectedUser(user)
   }
-  
+
   render() {
-    const {theme, isLoading, hasFeatures, hasHelps, hasOthers, data, featuresData, othersData} = this.state
-    
+    const { accountType } = this.state
+
     return (
       <div>
-        
-        {
-          isLoading && data.length <= 0 && (
-            <Box sx={{
-              margin: 0,
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              marginRight: '-50%',
-              transform: 'translate(-50%, -50%)'
-            }}>
-              <CircularProgress style={{color: Color.helpaDarkPink}}/>
-            </Box>
-          )
-        } 
         {
           accountType != null && (
-            <div className={selectedUser === 'agent' ? ' banner-agent' : 'banner-helpa'}>
-                {
-                  <Banner 
-                    {...this.props}
-                    theme={selectedUser}
-                  />
-                }
-                {
-                  hasHelps ? 
-                  <Video
-                  theme={selectedUser}
-                  data={data}
-                  />
-                  :
-                  ""
-                }
-                {
-                  featuresData.length > 0 ? 
-                  <Features 
-                  theme={selectedUser}
-                  data={featuresData}
-                  /> 
-                  : 
-                  ""
-                }
-                {
-                  othersData.length > 0 ? 
-                  <Others
-                  theme={selectedUser}
-                  data={othersData}
-                  />
-                  :
-                  ""
-                }
+            <div className={accountType === 'agent' ? ' banner-agent' : 'banner-helpa'}>
+              {
+                <Banner
+                  {...this.props}
+                  theme={accountType}
+                />
+              }
+
+              <Video
+                theme={accountType}
+                data={Helper.data[accountType].helps}
+              />
+              <Features
+                theme={accountType}
+                data={Helper.data[accountType].features}
+              />
+
+              <Others
+                theme={accountType}
+                data={Helper.data[accountType].others}
+              />
             </div>
           )
         }
@@ -103,12 +68,12 @@ class Landing extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({state: state});
-const mapDispatchToProps = (dispatch) =>{
+const mapStateToProps = (state) => ({ state: state });
+const mapDispatchToProps = (dispatch) => {
   const { actions } = require('reduxhandler');
   return {
     setColor: (type) => dispatch(actions.setColor(type)),
-    setSelectedUser: (user) => {dispatch(actions.setSelectedUser(user))},
+    setSelectedUser: (user) => { dispatch(actions.setSelectedUser(user)) },
   }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Landing))
--- src/modules/landing/video.js
+++ src/modules/landing/video.js
@@ -9,53 +9,53 @@ import API from 'services/api'
 import Routes from 'common/Routes'
 
 export class Video extends Component {
-    constructor(props) {
-      super(props)
-      this.state={
-        theme: this.props.theme,
-        data: this.props.data,
-        features: [],
-        hasFetched: false
-      }
+  constructor(props) {
+    super(props)
+    this.state = {
+      theme: this.props.theme,
+      data: this.props.data,
+      features: [],
+      hasFetched: false
     }
-    render(){
-        const {data, theme} = this.state;
-    return(
+  }
+  render() {
+    const { data, theme } = this.state;
+    return (
       <div>
         {/* Web */}
         <div className='web'>
           <div>
-          <Grid className='gridBg' style={
-            theme == 'agent' ? {backgroundImage: `url(${bgAgent})`, backgroundColor: '#F1F5FB'} : {backgroundImage: `url(${bgHelpa})`, backgroundColor: '#FFFAFC'}
-        } container alignItems={'center'} justifyContent={'center'} >
+            <Grid className='gridBg' style={
+              theme == 'agent' ? { backgroundImage: `url(${bgAgent})`, backgroundColor: '#F1F5FB' } : { backgroundImage: `url(${bgHelpa})`, backgroundColor: '#FFFAFC' }
+            } container alignItems={'center'} justifyContent={'center'} >
               <Grid item xs={6} style={{
                 padding: '5%',
                 textAlign: 'left'
               }}>
-                  <h1 className='h1-lg' style={{
-                    fontWeight: 'bold',
-                    fontSize: "64px",
-                    color: '#34475D',
-                    marginBottom: "10%"
-                  }}>
+                <h1 className='h1-lg' style={{
+                  fontWeight: 'bold',
+                  fontSize: "64px",
+                  color: '#34475D',
+                  marginBottom: "10%"
+                }}>
                   {
-                    theme == 'agent' ? 
-                    'What do you need a Helpa for?' 
-                    :
-                    'What kind of work can I help with?'
+                    theme == 'agent' ?
+                      'What do you need a Helpa for?'
+                      :
+                      'What kind of work can I help with?'
                   }
-                  </h1>
-                  <p style={{
-                    fontSize: "18px",
-                    color: '#34475DA3',
-                  }}>
-                    {
-                      theme == 'agent' ? 
+                </h1>
+                <p style={{
+                  fontSize: "18px",
+                  color: '#34475DA3',
+                }}>
+                  {
+                    theme == 'agent' ?
                       'Do you need help with your property opens this weekend? Need more manpower to complete your inspection reports? Let KeyHelpa find the freelance help you need.'
                       :
                       'We’ve got real estate agencies, agents and property managers looking for help from experienced people just like you. You’ll find a range of real estate industry - related jobs right here.'
-                    }
-                  </p>
+                  }
+                </p>
               </Grid>
               <Grid item style={{
                 display: 'flex',
@@ -63,113 +63,110 @@ export class Video extends Component {
               }} xs={6} textAlign={'right'} padding={'5%'} >
                 {
                   data.map((item, index) => {
-                  if(item.payload_value.helps != undefined){
-                    return(
-                    <div key={index} style={{
-                      display: 'flex',
-                      justifyContent: 'right',
-                      flexBasis: '100%',
-                    }}>
+                    return (
+                      <div key={index} style={{
+                        display: 'flex',
+                        justifyContent: 'right',
+                        flexBasis: '100%',
+                      }}>
+                        <Box sx={{
+                          display: 'flex',
+                          justifyContent: 'center',
+                          textAlign: 'right',
+                          flexDirection: 'column',
+                          marginRight: '25px'
+                        }}>
+                          <h3 style={{
+                            fontWeight: 'bold',
+                            fontSize: '20px',
+                            color: '#34475D'
+                          }} >{item.title}</h3>
+                          {(item.description).map((val, ctr) => {
+                            return (<p key={ctr}>{val}</p>)
+                          })}
+                        </Box>
+                        <Box sx={{
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          flexDirection: 'row'
+                        }}>
+                          <div style={{
+                            marginTop: '25px',
+                          }}>
+                            <div className='video' dangerouslySetInnerHTML={{ __html: item.url }}></div>
+                            {/* <iframe  width="400" height="300" src={`${item.payload_value.helps.url}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
+                          </div>
+                        </Box>
+                      </div>
+                    )
+                  })
+                }
+
+              </Grid>
+            </Grid>
+          </div>
+        </div>
+        {/* Mobile */}
+        <div className='mobile'>
+          <Grid className='gridBg' style={
+            theme == 'agent' ? { backgroundImage: `url(${bgAgent})`, backgroundColor: '#F1F5FB' } : { backgroundImage: `url(${bgHelpa})`, backgroundColor: '#FFFAFC' }
+          } container alignItems={'center'} justifyContent={'center'} >
+            <Grid item xs={10} style={{
+              padding: '5%',
+              textAlign: 'left'
+            }}>
+              <h1 className='videoHeader'>{theme == 'agent' ?
+                'What do you need a Helpa for?'
+                :
+                'What kind of work can I help with?'}</h1>
+              <h3 className='videoSubHeader'>{theme == 'agent' ?
+                'Do you need help with your property opens this weekend? Need more manpower to complete your inspection reports? Let KeyHelpa find the freelance help you need.'
+                :
+                'We’ve got real estate agencies, agents and property managers looking for help from experienced people just like you. You’ll find a range of real estate industry-related jobs right here.'}</h3>
+              {
+                data.map((item, index) => {
+                  return (
+                    <div>
+                      <Box sx={{
+                        justifyContent: 'center',
+                        textAlign: 'center',
+                        flexDirection: 'column',
+                        marginTop: '25px',
+                        marginBottom: '25px'
+                      }}>
+                        <div dangerouslySetInnerHTML={{ __html: item.url }}></div>
+                      </Box>
                       <Box sx={{
                         display: 'flex',
                         justifyContent: 'center',
-                        textAlign: 'right',
+                        textAlign: 'center',
                         flexDirection: 'column',
                         marginRight: '25px'
                       }}>
                         <h3 style={{
                           fontWeight: 'bold',
                           fontSize: '20px',
-                          color: '#34475D'
-                        }} >{item.payload_value.helps.title}</h3>
-                        {(item.payload_value.helps.description).map((val, ctr)=>{
-                          return(<p key={ctr}>{val}</p>)
-                        })}
-                      </Box>
-                      <Box sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center', 
-                        flexDirection: 'row'
-                      }}>
-                        <div style={{
-                            marginTop: '25px',
-                          }}>
-                            <div className='video'  dangerouslySetInnerHTML={{__html: item.payload_value.helps.url}}></div>
-                            {/* <iframe  width="400" height="300" src={`${item.payload_value.helps.url}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
-                          </div>
-                      </Box> 
-                    </div>
-                    )
-                  }
-                })
-                }
-              
-              </Grid>
-          </Grid>
-        </div>
-        </div>
-        {/* Mobile */}
-        <div className='mobile'>
-        <Grid className='gridBg' style={
-            theme == 'agent' ? {backgroundImage: `url(${bgAgent})`, backgroundColor: '#F1F5FB'} : {backgroundImage: `url(${bgHelpa})`, backgroundColor: '#FFFAFC'}
-        } container alignItems={'center'} justifyContent={'center'} >
-          <Grid item xs={10} style={{
-                padding: '5%',
-                textAlign: 'left'
-              }}>
-                <h1 className='videoHeader'>{theme == 'agent' ? 
-                'What do you need a Helpa for?' 
-                :
-                'What kind of work can I help with?'}</h1>
-                <h3 className='videoSubHeader'>{theme == 'agent' ? 
-                'Do you need help with your property opens this weekend? Need more manpower to complete your inspection reports? Let KeyHelpa find the freelance help you need.'
-                :
-                'We’ve got real estate agencies, agents and property managers looking for help from experienced people just like you. You’ll find a range of real estate industry-related jobs right here.'}</h3>
-                {
-                  data.map((item, index) => {
-                    if(item.payload_value.helps != undefined){
-                      return(
-                      <div>
-                        <Box sx={{
-                        justifyContent: 'center',
-                        textAlign: 'center',
-                        flexDirection: 'column',
-                        marginTop: '25px',
-                        marginBottom: '25px'
-                        }}>
-                          <div  dangerouslySetInnerHTML={{__html: item.payload_value.helps.url}}></div>
-                        </Box>
-                        <Box sx={{
-                          display: 'flex',
-                          justifyContent: 'center',
-                          textAlign: 'center',
-                          flexDirection: 'column',
-                          marginRight: '25px'
-                        }}>
-                        <h3 style={{
-                          fontWeight: 'bold',
-                          fontSize: '20px',
                           color: '#34475D',
                           marginTop: '25px'
-                        }} >{item.payload_value.helps.title}</h3>
-                        {(item.payload_value.helps.description).map((val, ctr)=>{
-                          return(<p key={ctr}>{val}</p>)
+                        }} >{item.title}</h3>
+                        {(item.description).map((val, ctr) => {
+                          return (<p key={ctr}>{val}</p>)
                         })}
                       </Box>
-                      </div>
-                    )}
-                  })
-                }
-                
-                  
-              </Grid>
+                    </div>
+                  )
+                })
+              }
+
+
+            </Grid>
           </Grid>
         </div>
       </div>
-      
-      );
-    }
+
+    );
+  }
 }
 
 export default Video
