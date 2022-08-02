2c643f52bdb69a5c742b59b175c3680e3bd3f5fe
kennck <kennettecanales@gmail.com>
2022-08-02 08:00:22 +0800

    update

--- src/modules/guides/data.js
+++ src/modules/guides/data.js
@@ -4,51 +4,88 @@ export default {
     agent: [{
         title: 'Create account',
         icon: Person,
-        description: "Get started  with KeyHelpa and hire Helpa when you need help."
+        description: "Get started  with KeyHelpa and hire Helpa when you need help.",
+        route: '/agents/create_account'
     }, {
         title: 'Set up profile',
         icon: Person,
-        description: "Complete your profile setup to access all features of KeyHelpa."
+        description: "Complete your profile setup to access all features of KeyHelpa.",
+        route: '/agents/setup_profile'
     }, {
         title: 'Search helpa',
         icon: Person,
-        description: "Look for the specific Helpa that match with job you’re hiring."
+        description: "Look for the specific Helpa that match with job you’re hiring.",
+        route: '/agents/search_helpa'
     }, {
         title: 'Create job posting',
         icon: Person,
-        description: "Start your job posting to attract, to invite and to hire Helpa."
+        description: "Start your job posting to attract, to invite and to hire Helpa.",
+        route: '/agents/create_job_posting'
     }, {
         title: 'Send invite',
         icon: Person,
-        description: "Invite any  Helpa you like to check on your job post."
+        description: "Invite any  Helpa you like to check on your job post.",
+        route: '/agents/send_invite'
     }, {
         title: 'Accept proposal',
         icon: Person,
-        description: "Approve job proposals sent to you by Helpa"
+        description: "Approve job proposals sent to you by Helpa",
+        route: '/agents/accept_proposal'
     }, {
         title: 'Interview helpa',
         icon: Person,
-        description: "Schedule and invite a helpa over an interview to discuss the job."
+        description: "Schedule and invite a helpa over an interview to discuss the job.",
+        route: '/agents/interview_helpa'
     }, {
         title: 'Hire helpa',
         icon: Person,
-        description: "Let Helpa work on your projects by hiring them."
+        description: "Let Helpa work on your projects by hiring them.",
+        route: '/agents/hire_helpa'
     }, {
         title: 'End contract',
         icon: Person,
-        description: "Be guided on how to end the job contract between you and Helpa."
+        description: "Be guided on how to end the job contract between you and Helpa.",
+        route: '/agents/end_contract'
     }, {
         title: 'Pause contract',
         icon: Person,
-        description: "Be guided on how to pause the job contract between you and Helpa."
+        description: "Be guided on how to pause the job contract between you and Helpa.",
+        route: '/agents/pause_contract'
     }, {
         title: 'Dispute contract',
         icon: Person,
-        description: "Learn how to open dispute on the contract between you and Helpa."
+        description: "Learn how to open dispute on the contract between you and Helpa.",
+        route: '/agents/dispute_contract'
     }, {
         title: 'Edit basic info',
         icon: Person,
-        description: "Be guided on how to update your basic information."
+        description: "Be guided on how to update your basic information.",
+        route: '/agents/edit_basic_info'
+    }, {
+        title: 'Edit agency info',
+        icon: Person,
+        description: "Be guided on how to update your agency information.",
+        route: '/agents/edit_agency_info'
+    }, {
+        title: 'Update bank details',
+        icon: Person,
+        description: "Managed authorized bank details you connected with KeyHelpa.",
+        route: '/agents/update_bank_details'
+    }, {
+        title: 'Update password',
+        icon: Person,
+        description: "Make sure to keep your password private and secured.",
+        route: '/agents/update_password'
+    }, {
+        title: 'Manage security',
+        icon: Person,
+        description: "Set securities to your KeyHelpa account for extra protection.",
+        route: '/agents/manage_security'
+    }, {
+        title: 'Manage notifications',
+        icon: Person,
+        description: "Set your notifications to received any kind of updates you want.",
+        route: '/agents/manage_notifications'
     }],
     helpa: [{
         title: 'Create account',
