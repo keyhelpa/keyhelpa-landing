a1e24c9634fc1a9f415870bfbcc8ca53756b6f22
kennck <kennettecanales@gmail.com>
2022-08-02 08:41:44 +0800

    udpate

--- src/modules/guides/index.js
+++ src/modules/guides/index.js
@@ -3,12 +3,10 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Banner from './banner'
 import Colors from 'common/Colors';
-import AgentVideoThumbnail from 'assets/agent-video-thumbnail.png'
-import HelpaVideoThumbnail from 'assets/helpa-video-thumbnail.png'
-
 import { SvgIcon } from '@mui/material';
-import { PlayArrow, PlayCircleFilled } from '@mui/icons-material';
 import Data from './data'
+import Introduction from './pages/introduction'
+
 class Guide extends Component {
     constructor(props) {
         super(props)
@@ -31,48 +29,15 @@ class Guide extends Component {
         })
     }
 
-    renderVideo() {
+    managePage(item){
         const { theme } = this.state;
-
-        return (
-            <div style={{
-                width: '100%',
-                float: 'left',
-                height: 500,
-                backgroundImage: `url(${theme == 'agent' ? AgentVideoThumbnail : HelpaVideoThumbnail})`,
-                alignItems: 'center',
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: '100% auto',
-                display: 'flex',
-                alignContent: 'center',
-                justifyContent: 'center'
-            }}
-                className="active-color-hover full-width-mobile video-player-holder"
-            >
-                <div style={{
-                    width: '100px',
-                    height: '100px',
-                    borderRadius: '50px',
-                    float: 'left',
-                    background: theme == 'agent' ? Colors.agentGray : Colors.helpaPink,
-                    alignItems: 'center',
-                    alignContent: 'center',
-                    display: 'flex',
-                    justifyContent: 'center'
-                }}
-                >
-                    <SvgIcon
-                        component={PlayArrow}
-                        style={{
-                            color: Colors.white,
-                            fontSize: 60
-                        }}
-                    />
-                </div>
-            </div>
-        )
+        switch(item.route){
+            case '/agent/guides': return <Introduction theme={theme}/>
+            case '/helpa/guides': return <Introduction theme={theme}/>
+        }
     }
 
+    
     renderMenu() {
         const { theme, menu } = this.state;
         return (
@@ -178,16 +143,9 @@ class Guide extends Component {
                         className="full-width-mobile-with-margin"
                     >
 
-                        <p style={{
-                            color: theme == 'agent' ? Colors.agentTextTitle : Colors.helpaTextTitle
-                        }}>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                        </p>
-
                         {
-                            this.renderVideo()
+                            item && this.managePage(item)
                         }
-
                         {
                             this.renderMenu()
                         }
