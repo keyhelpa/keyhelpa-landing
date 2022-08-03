405d081758d917448e372d01064ec018e3c36b5c
kennck <kennettecanales@gmail.com>
2022-08-03 09:23:15 +0800

    update

--- src/modules/faq/index.js
+++ src/modules/faq/index.js
@@ -3,13 +3,17 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Colors from 'common/Colors';
 import { SvgIcon } from '@mui/material';
+import Banner from '../guides/banner'
+import Data from './data'
+import { ArrowDropDown, ArrowUpward, KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
 
-class Guide extends Component {
+class Stack extends Component {
     constructor(props) {
         super(props)
         this.state = {
             theme: 'agent',
-            accountType: null
+            accountType: null,
+            data: []
         }
     }
 
@@ -18,8 +22,88 @@ class Guide extends Component {
         let user = history.location.pathname.includes('agent') ? 'agent' : 'helpa'
         this.setState({
             accountType: user,
-            theme: user
+            theme: user,
+            data: Data.data
         })
+    }
+    manageSelected(selectedItem, selectedIndex) {
+        const { data } = this.state;
+        let newData = data.map((item, index) => {
+            if (selectedIndex == index) {
+                return {
+                    ...item,
+                    open: !selectedItem.open
+                }
+            }
+            return item
+        })
+        this.setState({
+            data: newData
+        })
+    }
+
+    renderData() {
+        const { data } = this.state;
+        return (
+            <div style={{
+                float: 'left',
+                width: '100%'
+            }}>
+                <ul style={{
+                    width: '100%',
+                    margin: 0,
+                    padding: 0,
+                    listStyle: 'none'
+                }}>
+                    {
+                        data.map((item, index) => (
+                            <li
+
+                                style={{
+                                    paddingLeft: 20,
+                                    paddingTop: 10,
+                                    paddingBottoM: 10,
+                                    paddingRight: 20,
+                                    width: '100%',
+                                    float: 'left',
+                                    borderBottom: 'solid 1px ' + Colors.lightGray,
+                                    borderBottomLeftRadius: 5,
+                                    borderBottomRightRadius: 5
+                                }}
+                                onClick={() => {
+                                    this.manageSelected(item, index)
+                                }}
+                                className="cursor-hover"
+                            >
+                                <span style={{
+                                    width: '100%',
+                                    float: 'left',
+                                    marginBottom: 20,
+                                    display: 'flex',
+                                    justifyContent: 'space-between',
+                                    marginTop: 20
+                                }}>
+                                    <b style={{
+                                    }}>
+                                        {
+                                            item.title
+                                        }
+                                    </b>
+                                    <SvgIcon
+                                        component={item.open ? KeyboardArrowUp : KeyboardArrowDown}
+                                    />
+                                </span>
+                                {
+                                    item.open && (
+                                        item.description()
+                                    )
+                                }
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        )
     }
 
     render() {
@@ -30,6 +114,34 @@ class Guide extends Component {
                 float: 'left',
                 backgroundColor: theme == 'agent' ? Colors.agentBackgroundColor : Colors.helpaBackgroundColor,
             }}>
+                <Banner
+                    title={'FAQs'}
+                    theme={theme}
+                />
+
+                <div style={{
+                    backgroundColor: theme == 'agent' ? Colors.agentBackgroundColor : Colors.helpaBackgroundColor,
+                    float: 'left',
+                    width: '100%',
+                    minHeight: '100vh',
+                    marginBottom: 100
+                }}>
+                    <div style={{
+                        width: '50%',
+                        float: 'left',
+                        marginLeft: '25%',
+                        marginRight: '25%',
+                        paddingTop: 50,
+                        paddingBottom: 50
+                    }}
+                        className="full-width-mobile-with-margin"
+                    >
+                        {
+                            this.renderData()
+                        }
+                    </div>
+
+                </div>
             </div>
         )
     }
@@ -40,4 +152,4 @@ const mapDispatchToProps = (dispatch) => {
     return {
     }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Guide));
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Stack));
