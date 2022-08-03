d661d4ca42f7f008c74dc385546e215e460f0327
kennck <kennettecanales@gmail.com>
2022-08-03 08:35:39 +0800

    update

--- src/modules/landing/index.js
+++ src/modules/landing/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import Features from 'modules/landing/features.js'
 import Video from 'modules/landing/video.js'
 import Others from 'modules/landing/others.js'
@@ -15,99 +15,88 @@ import Colors from 'common/Colors'
 import AgentLogoBackground from 'assets/lighterGray.png'
 import HelpaLogoBackground from 'assets/lighterPink.png'
 
-class Landing extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      theme: 'helpa',
-      accountType: null
-    }
-  }
+function Landing(props) {
+  const [theme, setTheme] = useState('helpa')
+  const { setColor, setSelectedUser, history } = props
+  const { accountType } = props.state;
 
-  componentDidMount() {
-    const { setColor, setSelectedUser, history } = this.props
+  useEffect(() => {
     let user = history.location.pathname.includes('agent') ? 'agent' : 'helpa'
-    this.setState({
-      accountType: user
-    })
+    setTheme(user)
     setColor(user)
     setSelectedUser(user)
-  }
+  }, [props.history]);
 
-  render() {
-    const { accountType } = this.state
+  return (
+    <div>
+      {
+        accountType != null && (
+          <div className={accountType === 'agent' ? ' banner-agent' : 'banner-helpa'}>
 
-    return (
-      <div>
-        {
-          accountType != null && (
-            <div className={accountType === 'agent' ? ' banner-agent' : 'banner-helpa'}>
+            {
+              <Banner
+                {...props}
+                theme={accountType}
+              />
+            }
+            <div style={{
+              width: '100%',
+              float: 'left',
+              background: accountType == 'agent' ? Colors.agentBackgroundColor : Colors.helpeBackgroundColor
+            }}>
 
-              {
-                <Banner
-                  {...this.props}
-                  theme={accountType}
-                />
-              }
               <div style={{
+                backgroundImage: `url(${accountType == 'agent' ? AgentLogoBackground : HelpaLogoBackground})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'auto 60%',
+                backgroundRepeat: 'no-repeat',
                 width: '100%',
                 float: 'left',
-                background: accountType == 'agent' ? Colors.agentBackgroundColor : Colors.helpeBackgroundColor
+                paddingLeft: '5%',
+                paddingRight: '5%',
+              }}>
+                <Video
+                  theme={accountType}
+                  data={Helper.data[accountType].helps}
+                />
+              </div>
+              <div style={{
+                backgroundImage: `url(${accountType == 'agent' ? AgentLogoBackground : HelpaLogoBackground})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'auto 60%',
+                backgroundRepeat: 'no-repeat',
+                width: '100%',
+                float: 'left',
+                paddingLeft: '5%',
+                paddingRight: '5%',
+              }}>
+                <Features
+                  theme={accountType}
+                  data={Helper.data[accountType].features}
+                />
+              </div>
+              <div style={{
+                // backgroundImage: `url(${accountType == 'agent' ? AgentLogoBackground : HelpaLogoBackground})`,
+                // backgroundPosition: 'center',
+                // backgroundSize: 'auto 60%',
+                // backgroundRepeat: 'no-repeat',
+                width: '100%',
+                float: 'left',
+                paddingLeft: '5%',
+                paddingRight: '5%',
               }}>
 
-                <div style={{
-                  backgroundImage: `url(${accountType == 'agent' ? AgentLogoBackground : HelpaLogoBackground})`,
-                  backgroundPosition: 'center',
-                  backgroundSize: 'auto 60%',
-                  backgroundRepeat: 'no-repeat',
-                  width: '100%',
-                  float: 'left',
-                  paddingLeft: '5%',
-                  paddingRight: '5%',
-                }}>
-                  <Video
-                    theme={accountType}
-                    data={Helper.data[accountType].helps}
-                  />
-                </div>
-                <div style={{
-                  backgroundImage: `url(${accountType == 'agent' ? AgentLogoBackground : HelpaLogoBackground})`,
-                  backgroundPosition: 'center',
-                  backgroundSize: 'auto 60%',
-                  backgroundRepeat: 'no-repeat',
-                  width: '100%',
-                  float: 'left',
-                  paddingLeft: '5%',
-                  paddingRight: '5%',
-                }}>
-                  <Features
-                    theme={accountType}
-                    data={Helper.data[accountType].features}
-                  />
-                </div>
-                <div style={{
-                  // backgroundImage: `url(${accountType == 'agent' ? AgentLogoBackground : HelpaLogoBackground})`,
-                  // backgroundPosition: 'center',
-                  // backgroundSize: 'auto 60%',
-                  // backgroundRepeat: 'no-repeat',
-                  width: '100%',
-                  float: 'left',
-                  paddingLeft: '5%',
-                  paddingRight: '5%',
-                }}>
-
-                  <Others
-                    theme={accountType}
-                    data={Helper.data[accountType].others}
-                  />
-                </div>
+                <Others
+                  theme={accountType}
+                  data={Helper.data[accountType].others}
+                />
               </div>
             </div>
-          )
-        }
-      </div>
-    )
-  }
+          </div>
+        )
+      }
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => ({ state: state });
