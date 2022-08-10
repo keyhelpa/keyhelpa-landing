87bf76e41be8902946ea9c28cb6508782d3cd933
kmlcburce <45752669+kmlcburce@users.noreply.github.com>
2022-08-10 17:46:33 +0800

    update

--- src/modules/guides/pages/agent/manage_notif.js
+++ src/modules/guides/pages/agent/manage_notif.js
@@ -53,6 +53,35 @@ class ManageNotifAgent extends Component {
         )
     }
 
+    renderContent(){
+        return(
+            <div>
+                <p>On Agent’s web application, click your profile icon or photo then a dropdown menu will appear. Choose “Settings” and you will be taken to a new sidebar menu. Click the “Notifications” on your sidebar menu and you will be redirected to the “Notifications” page.</p>
+                <p>Agents can set where to receive notifications either via Email, Push, or SMS. The notifications that an Agent can received are categorized into two categories:</p>
+                <ol type='1'>
+                    <li><b>Messages</b> - these are notifications that can be received by Agents that are outside contracts or non-contract related such as::</li>
+                    <ul>
+                        <li>A job invitation is received</li>
+                        <li>An invitation has been received</li>
+                        <li>An interview is accepted or modified</li>
+                        <li>An interview or offer is declined or withdrawn</li>
+                        <li>An offer is accepted</li>
+                        <li>An offer is rejected</li>
+                        <li>A job posting will expire soon</li>
+                        <li>A job posting expired</li>
+                    </ul>
+                    <li><b>Contracts</b> - these are notifications that can be received by Agents are contract-related such as:</li>
+                    <ul>
+                        <li>Freelancer accepted terms</li>
+                        <li>Freelancer requested changes</li>
+                        <li>Other contract-related messages</li>
+                        <li>Communications from KeyHelpa</li>
+                    </ul>
+                </ol>
+                <p>If you have any concerns or inquiries, please don’t hesitate to <a href='#'>contact us</a>. </p>
+            </div>
+        )
+    }
 
     render() {
         const { theme } = this.props;
@@ -64,12 +93,14 @@ class ManageNotifAgent extends Component {
                 <p style={{
                     color: theme == 'agent' ? Colors.agentTextTitle : Colors.helpaTextTitle
                 }}>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
+ A notification email relays an important message to both Helpa and Agent. It is important for them to get notified what has been going on with their jobs, contracts, and any activities they do in KeyHelpa.                </p>
 
 
                 {
                     this.renderVideo()
+                }
+                {
+                    this.renderContent()
                 }
             </div>
         )
