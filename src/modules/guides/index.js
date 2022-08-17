0ba55dc6cf910dffa96084eee20f717a4820c3b5
kmlcburce <45752669+kmlcburce@users.noreply.github.com>
2022-08-17 17:12:06 +0800

    update

--- src/modules/guides/index.js
+++ src/modules/guides/index.js
@@ -137,8 +137,7 @@ class Guide extends Component {
                     gap: '5%'
                 }}>
                     {
-                        menu.map((item) => (
-                            
+                        menu.map((item) => {
                             <div style={{
                                 width: '30%',
                                 float: 'left',
@@ -181,8 +180,8 @@ class Guide extends Component {
                                         item.description
                                     }
                                 </p>
-                            </div>
-                        ))
+                            </div>}
+    )
                     }
                 </div>
             </div>
